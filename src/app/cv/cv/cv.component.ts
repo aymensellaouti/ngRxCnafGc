f41ef9b543b14cdc1a726ea78dfb6a08c4a3a685
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-18 13:29:33 +0100

    retry et catchError

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -2,21 +2,33 @@ import { Component } from "@angular/core";
 import { Cv } from "../model/cv";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
+import { Observable, catchError, of, retry } from "rxjs";
 @Component({
-  selector: 'app-cv',
-  templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css'],
+  selector: "app-cv",
+  templateUrl: "./cv.component.html",
+  styleUrls: ["./cv.component.css"],
 })
 export class CvComponent {
-  cvs: Cv[] = [];
+  cvs$: Observable<Cv[]>;
   selectedCv: Cv | null = null;
   date = new Date();
 
-  constructor(
-    private toastr: ToastrService,
-    private cvService: CvService
-  ) {
-    this.cvService.getCvs().subscribe({
+  constructor(private toastr: ToastrService, private cvService: CvService) {
+    this.cvs$ = this.cvService.getCvs().pipe(
+      retry({
+        delay: 1500,
+        count: 4,
+      }),
+      /* Opérateur qui permet d'intercepter les erreurs */
+      catchError((e) => {
+        this.toastr.error(`
+          Attention!! Les données sont fictives, problème avec le serveur.
+          Veuillez contacter l'admin.`);
+        return of(this.cvService.getFakeCvs());
+      })
+    );
+
+    /* .subscribe({
       next: (cvs) => {
         this.cvs = cvs;
       },
@@ -26,10 +38,10 @@ export class CvComponent {
           Attention!! Les données sont fictives, problème avec le serveur.
           Veuillez contacter l'admin.`);
       },
-    });
+    }); */
   }
 
-  getSelectedCv(cv:Cv) {
+  getSelectedCv(cv: Cv) {
     this.selectedCv = cv;
   }
 }
